b45b62c40d76d2483eb07e537a2796d8a7babb70
Zeke Sikelianos <zeke@sikelianos.com>
2015-12-15 22:41:53 -0600

    standard, es6, node 4, fancy headings, one point oh.

--- index.js
+++ index.js
@@ -1,39 +1,37 @@
 #!/usr/bin/env node
 
-var fs = require("fs")
-var marked = require("marked")
-var glob = require("glob")
-var toc = require('marked-toc');
-
-var dir = process.argv[2]
+const fs = require('fs')
+const path = require('path')
+const glob = require('glob')
+const toc = require('marked-toc')
+const inflection = require('inflection')
+const dir = process.argv[2]
 
 if (!dir) {
-  console.log("\nUsage: \nmarkdown-index directory/fulla/markdowns\n")
+  console.log('\nUsage: \nmarkdown-index directory/fulla/markdowns\n')
   process.exit(1)
 }
 
-// Recursively read all markdown files
-glob(dir + "/**/*.md", function (err, files) {
-  // console.log(files);
-  var tables = files.map(function(file) {
+glob(dir + '/**/*.md', function (err, files) {
+  if (err) throw err
+  var tables = files.map(function (file) {
+    var table = toc(fs.readFileSync(file, 'utf8'))
 
-    // Create table of contents
-    var table = toc(fs.readFileSync(file).toString(), 'utf8')
+    if (file.match('index.md')) return
+    if (file.match('README.md')) return
+    if (file.match('node_modules')) return
+    if (table.length < 10) return
 
-    if (file.match('index.md')) return;
-    // if (file.match('README.md')) return;
-    if (file.match('node_modules')) return;
-    if (table.length < 10) return;
+    var prettyFile = inflection.titleize(path.parse(file).name.replace(/(_|-)/g, ' '))
 
     // Add filename as a heading
-    table = "### [" + file + "](" + file + ")\n\n" + table
+    table = '### [' + prettyFile + '](' + file + ')\n\n' + table
 
     // Prepend filename to links
-    table = table.replace(/\(#/g, "(" + file + "#")
-    return table;
+    table = table.replace(/\(#/g, '(' + file + '#')
+    return table
   })
 
-  process.stdout.write(tables.join("\n\n"));
-  return tables.join("\n\n");
-
+  process.stdout.write(tables.join('\n'))
+  return tables.join('\n\n')
 })
